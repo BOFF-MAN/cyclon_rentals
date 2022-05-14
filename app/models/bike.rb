abec4995dae08838e07a5f616a1e644ebd974b00
yuvalzoref <yuval_zoref@hotmail.com>
2022-05-14 11:34:40 +0100

    adding geocode model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,8 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   validates :daily_rate, presence: true
