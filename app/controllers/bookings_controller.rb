7c2c439df7064f1b98bc56b2fff8155c788f5c58
david bradford <db5382@gmail.com>
2022-05-14 14:51:37 +0100

    work on bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,33 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
 
   def create
-     bike = Bike.find(params[:bike_id])
+    @bike = Bike.find(params[:bike_id])
+    @user = User.find(params[:user_id])
+    @booking = Booking.new(booking_params)
+    @booking.bike = @bike
+    @booking.user = @user
+    start_date = Date.parse(bookings_params[:start_date])
+    end_date = Date.parse(bookings_params[:end_date])
+    total_days = (end_date - start_date).to_i + 1
+    total_price = total_days * @bike.daily_rate
+    @booking.total = total_price
 
-     if current_user == room.user
-        flash[:alert] = "Cannot book your own bike!"
-     else
-      start_date = Date.parse(bookings_params[:start_date])
-      end_date = Date.parse(bookings_params[:end_date])
-      total = (end_date - start_date).to_i + 1
-     end
+    if current_user == @bike.user
+      flash[:alert] = "Cannot book your own bike!"
+      render :new
+    elsif @booking.save
+      redirect_to bikes_path
+    else
+      render :new
+    end
   end
 end
