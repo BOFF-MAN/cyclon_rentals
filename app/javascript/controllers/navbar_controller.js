5bbcfa70a071e4136d2145f5ae173ad0613ba6f1
Jonno Clifford <jma.clifford@gmail.com>
2022-05-19 21:38:46 +0100

    Home page cards changed to bike categories, spacing changed

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateNavbar() {
-    if (window.scrollY >= (window.innerHeight/2) ) {
+    if (window.scrollY >= (window.innerHeight/4) ) {
       this.element.classList.add("navbar-cyclon-white")
     } else {
       this.element.classList.remove("navbar-cyclon-white")
